1a62cc287f5f2907ab5d9aa13edb1bf181ce5d33
Xa1n <xhs893@gmail.com>
2019-06-12 13:45:39 +0100

    Add MapView zoom-to-London animation to ViewController.swift
    Co-authored-by: Callum Marshall <callum_marshall@hotmail.co.uk>

--- Breathe/ViewController.swift
+++ Breathe/ViewController.swift
@@ -7,12 +7,39 @@
 //
 
 import UIKit
+import Mapbox
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MGLMapViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let mapView = MGLMapView(frame: view.bounds)
+        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        mapView.delegate = self
+        
+        mapView.styleURL = MGLStyle.streetsStyleURL
+        
+        // coordinates for London, UK
+        let center = CLLocationCoordinate2D(latitude: 51.509865, longitude: -0.118092)
+        
+        // set starting point
+        mapView.setCenter(center, zoomLevel: 6, direction: 0, animated: false)
+        
+        view.addSubview(mapView)
+    }
+    
+    func mapViewDidFinishLoadingMap(_ mapView: MGLMapView) {
+        // Wait for the map to load before initiating first camera movement
+        
+        // Create a camera that rotates around center point
+        
+        let camera = MGLMapCamera(lookingAtCenter: mapView.centerCoordinate, altitude: 100000,
+                                  pitch: 3, heading: 0)
+        
+        // Animate the camera movement over 5 seconds
+        mapView.setCamera(camera, withDuration: 3, animationTimingFunction:
+            CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut))
     }
 
 
