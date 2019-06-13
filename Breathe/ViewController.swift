09d7c6c04903aaa38e8745a96b239b7f85e04138
Callum Marshall <callum_marshall@hotmail.co.uk>
2019-06-13 14:20:26 +0100

    add textview and button with connection to placeholder JSON

--- Breathe/ViewController.swift
+++ Breathe/ViewController.swift
@@ -11,37 +11,59 @@ import Mapbox
 
 class ViewController: UIViewController, MGLMapViewDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        let mapView = MGLMapView(frame: view.bounds)
-        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        mapView.delegate = self
+    @IBOutlet weak var textView: UITextView!
+    
+    private let networkingClient = NetworkingClient()
+    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view.
+//
+//        let mapView = MGLMapView(frame: view.bounds)
+//        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+//        mapView.delegate = self
+//
+//        mapView.styleURL = MGLStyle.streetsStyleURL
+//
+//        // coordinates for London, UK
+//        let center = CLLocationCoordinate2D(latitude: 51.509865, longitude: -0.118092)
+//
+//        // set starting point
+//        mapView.setCenter(center, zoomLevel: 6, direction: 0, animated: false)
+//
+//        view.addSubview(mapView)
+//        view.addSubview(textView)
+//    }
+//
+//    func mapViewDidFinishLoadingMap(_ mapView: MGLMapView) {
+//        // Wait for the map to load before initiating first camera movement
+//
+//        // Create a camera that rotates around center point
+//
+//        let camera = MGLMapCamera(lookingAtCenter: mapView.centerCoordinate, altitude: 100000,
+//                                  pitch: 3, heading: 0)
+//
+//        // Animate the camera movement over 5 seconds
+//        mapView.setCamera(camera, withDuration: 3, animationTimingFunction:
+//            CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut))
+//    }
+    
+    
+    @IBAction func executeRequest(_ sender: Any) {
         
-        mapView.styleURL = MGLStyle.streetsStyleURL
+        guard let urlToExecute = URL(string: "https://jsonplaceholder.typicode.com/posts/1") else {
+            return
+        }
         
-        // coordinates for London, UK
-        let center = CLLocationCoordinate2D(latitude: 51.509865, longitude: -0.118092)
-        
-        // set starting point
-        mapView.setCenter(center, zoomLevel: 6, direction: 0, animated: false)
-        
-        view.addSubview(mapView)
+        networkingClient.execute(urlToExecute) { (json, error) in
+            if let error = error {
+                self.textView.text = error.localizedDescription
+            } else if let json = json {
+                self.textView.text = json.description
+            }
+        }
     }
     
-    func mapViewDidFinishLoadingMap(_ mapView: MGLMapView) {
-        // Wait for the map to load before initiating first camera movement
-        
-        // Create a camera that rotates around center point
-        
-        let camera = MGLMapCamera(lookingAtCenter: mapView.centerCoordinate, altitude: 100000,
-                                  pitch: 3, heading: 0)
-        
-        // Animate the camera movement over 5 seconds
-        mapView.setCamera(camera, withDuration: 3, animationTimingFunction:
-            CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut))
-    }
-
 
 }
 
