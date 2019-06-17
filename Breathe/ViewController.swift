ab2f617e553853ca29732e5f96ce62148a757cea
Kiah Hewitt <hewittkiah@gmail.com>
2019-06-17 10:53:27 +0100

    added annotation styling and borough title
    Co-authored-by: Yadira Sanchez <yadi.sb1@gmail.com>
    Co-authored-by: Xain Storey <xhs893@gmail.com>
    Co-authored-by: Callum Marshall <callum_marshall@hotmail.co.uk>

--- Breathe/ViewController.swift
+++ Breathe/ViewController.swift
@@ -59,7 +59,7 @@ class ViewController: UIViewController, MGLMapViewDelegate {
         for coordinate in coordinates {
             let point = MGLPointAnnotation()
             point.coordinate = coordinate
-            point.title = "\(coordinate.latitude), \(coordinate.longitude)"
+            point.title = "Tower Hamlets"
             pointAnnotations.append(point)
         }
         
@@ -73,7 +73,21 @@ class ViewController: UIViewController, MGLMapViewDelegate {
         
         let reuseIdentifier = "\(annotation.coordinate.longitude)"
         
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseIdentifier)
         
+        if annotationView == nil {
+            annotationView = CustomAnnotationView(reuseIdentifier: reuseIdentifier)
+            annotationView!.bounds = CGRect(x: 0, y: 0, width: 40, height: 40)
+            
+            let hue = CGFloat(annotation.coordinate.longitude) / 100
+            annotationView!.backgroundColor = UIColor(hue: hue, saturation: 0.5, brightness: 1, alpha: 1)
+        }
+        
+        return annotationView
+    }
+    
+    func mapView(_ mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
+        return true
     }
 
     func mapViewDidFinishLoadingMap(_ mapView: MGLMapView) {
@@ -89,4 +103,25 @@ class ViewController: UIViewController, MGLMapViewDelegate {
             CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut))
     }
     
+}
+
+class CustomAnnotationView: MGLAnnotationView {
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        // Use CALayer’s corner radius to turn this view into a circle.
+        layer.cornerRadius = bounds.width / 2
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.white.cgColor
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Animate the border width in/out, creating an iris effect.
+        let animation = CABasicAnimation(keyPath: "borderWidth")
+        animation.duration = 0.1
+        layer.borderWidth = selected ? bounds.width / 4 : 2
+        layer.add(animation, forKey: "borderWidth")
+    }
 }
