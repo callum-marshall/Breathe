6fe81cded2413f632ef438948592e3264e37c3c4
Kiah Hewitt <hewittkiah@gmail.com>
2019-06-18 11:41:16 +0100

    add controllers, navigation bar and toggle button
    Co-authored-by: Callum Marshall <callum_marshall@hotmail.co.uk>

---
+++ Breathe/Controller/ContainerController.swift
@@ -0,0 +1,42 @@
+//
+//  ContainerController.swift
+//  Breathe
+//
+//  Created by Kiah Hewitt on 18/06/2019.
+//  Copyright © 2019 Student. All rights reserved.
+//
+
+import UIKit
+
+class ContainerController: UIViewController {
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - Init
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureHomeController()
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    // MARK: - Handlers
+    
+    func configureHomeController() {
+        let homeController = HomeController()
+        let controller = UINavigationController(rootViewController: homeController)
+        
+        view.addSubview(controller.view)
+        addChild(controller)
+        controller.didMove(toParent: self)
+    }
+    
+    func configureMenuController() {
+    
+    }
+    
+}
---
+++ Breathe/Controller/MenuController.swift
@@ -0,0 +1,23 @@
+//
+//  MenuController.swift
+//  Breathe
+//
+//  Created by Kiah Hewitt on 18/06/2019.
+//  Copyright © 2019 Student. All rights reserved.
+//
+
+import UIKit
+
+class MenuController: UIViewController {
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - Init
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Handlers
+}
